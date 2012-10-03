191949661f67e73c1b79299ee9cbf51cfeeeac65
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-10-03 22:10:51 +0100

    Beginings of pooling functionality.

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 var Connection = require('tedious').Connection;
 var util = require('util');
 
-function PooledConnection(config) {
+function PooledConnection(pool, config) {
     Connection.call(this, config);
+    this.pool = pool;
 }
 
 util.inherits(PooledConnection, Connection);
 
 PooledConnection.prototype.close = function() {
-    this._close();
+    this.emit('end');
+    this.pool.connectionAvailable(this);
 }
 
 PooledConnection.prototype._close = function() {
     Connection.prototype.close.call(this);
 }
 
-function ConnectionPool(config) {
+function ConnectionPool(poolConfig, connectionConfig) {
+  var pool = this;
+
+  pool.config = poolConfig;
+  pool.activeConnections = [];
+  pool.availableConnections = [];
+
+  pool.connectionAvailable = function(connection) {
+    this.activeConnections.splice(this.activeConnections.indexOf(connection), 1);
+  };
+
   return {
-    getConnection: function() {
-      return new PooledConnection(config);
+    Connection: function() {
+      var connection = new PooledConnection(pool, connectionConfig);
+      pool.activeConnections.push(connection);
+
+      return connection;
     }
   };
 };
--- test/connection-pool.test.js
+++ test/connection-pool.test.js
@@ -1,7 +1,7 @@
 var assert = require('assert')
 var ConnectionPool = require('../index');
 
-var config = {
+var connectionConfig = {
   userName: 'test',
   password: 'test',
   server: '192.168.1.212',
@@ -11,22 +11,25 @@ describe('ConnectionPool', function() {
   describe('one connection', function() {
     it('should connect', function(done) {
 
-      // var connection = new Connection(config);
+      // var connection = new Connection(connectionConfig);
 
       // connection.on('connect', function(err) {
       //   done();
       // });
 
-      var cp = new ConnectionPool(config);
-      var connection = cp.getConnection();
-      // console.log(connection);
+      var cp = new ConnectionPool({size: 2}, connectionConfig);
+      var Connection = cp.Connection;
+      //var connection = cp.getConnection();
+      var connection = new Connection();
+      //console.log(connection.pool);
 
       connection.on('connect', function(err) {
         connection.close();
+        // done();
       });
 
       connection.on('end', function(err) {
-        console.log('end')
+        // console.log('end')
         done();
       });
     })
